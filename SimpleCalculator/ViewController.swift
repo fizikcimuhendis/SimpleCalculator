28bd93f675f2624b13251e18e9a274c29b805ffa
Emir Hardal <emirhardal@me.com>
2019-11-08 15:56:35 +0300

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Emir Hardal on 8.11.2019.
+//  Copyright © 2019 Emir Hardal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
