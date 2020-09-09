29376fac24eb7192bd54073b0fe6278553ee6f9c
fizikcimuhendis <emirhardal@gmail.com>
2020-09-09 19:29:58 +0300

    Version 1.1

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -9,12 +9,91 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    // Global Variables
+    var result = Double()
+    let formatter = NumberFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // Summation
+    @IBAction func sumTapped(_ sender: Any) {
+        
+        if let firstNumber = Double(firstText.text!){
+            if let secondNumber = Double(secondText.text!){
+                
+                result = firstNumber + secondNumber
+                formatter.maximumFractionDigits = 4
+                formatter.minimumFractionDigits = 2
+                
+                let formattedResult = formatter.string(for: result)
+                               
+                resultLabel.text = formattedResult
+                
+                
+            }
+        }
+    }
+    
+    // Substraction
+    @IBAction func minusTapped(_ sender: Any) {
+        
+        if let firstNumber = Double(firstText.text!){
+            if let secondNumber = Double(secondText.text!){
+                
+                result = firstNumber - secondNumber
+                formatter.maximumFractionDigits = 4
+                formatter.minimumFractionDigits = 2
+                
+                let formattedResult = formatter.string(for: result)
+                               
+                resultLabel.text = formattedResult
+                
+            }
+        }
+    }
+    
+    // Multiplication
+    @IBAction func multiplyTapped(_ sender: Any) {
+        
+        if let firstNumber = Double(firstText.text!){
+            if let secondNumber = Double(secondText.text!){
+                
+                result = firstNumber * secondNumber
+                formatter.maximumFractionDigits = 4
+                formatter.minimumFractionDigits = 2
+                
+                let formattedResult = formatter.string(for: result)
+                
+                resultLabel.text = formattedResult
+                
+            }
+        }
+    }
+    
+    // Division
+    @IBAction func divideTapped(_ sender: Any) {
+        
+        if let firstNumber = Double(firstText.text!){
+            if let secondNumber = Double(secondText.text!){
+                
+                result = firstNumber / secondNumber
+                formatter.maximumFractionDigits = 4
+                formatter.minimumFractionDigits = 2
+                
+                let formattedResult = formatter.string(for: result)
+                
+                resultLabel.text = formattedResult
+                
+            }
+        }
+    }
 
 }
-
